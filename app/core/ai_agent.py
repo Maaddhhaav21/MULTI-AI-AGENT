349cb6fccd3c5fa87654fc657b307987537a581a
Madhav Manoj <maadhavmanoj21@gmail.com>
2026-08-29 21:31:52 +0530

    Ai agents redone

--- app/core/ai_agent.py
+++ app/core/ai_agent.py
@@ -1,35 +1,44 @@
 from langchain_groq import ChatGroq
 from langchain_community.tools.tavily_search import TavilySearchResults
-
 from langgraph.prebuilt import create_react_agent
-from langchain_core.messages.ai import AIMessage
+from langchain_core.messages import HumanMessage, AIMessage
 
 from app.config.settings import settings
 
-def get_response_from_ai_agents(llm_id , query , allow_search ,system_prompt):
 
-    llm = ChatGroq(model=llm_id)
+def get_response_from_ai_agents(llm_id, query, allow_search, system_prompt):
 
-    tools = [TavilySearchResults(max_results=2)] if allow_search else []
+    llm = ChatGroq(
+        model=llm_id,
+        api_key=settings.GROQ_API_KEY
+    )
+
+    tools = (
+        [TavilySearchResults(max_results=2)]
+        if allow_search
+        else []
+    )
 
     agent = create_react_agent(
         model=llm,
         tools=tools,
-        state_modifier=system_prompt
+        prompt=system_prompt
     )
 
-    state = {"messages" : query}
+    state = {
+        "messages": [
+            HumanMessage(content=query[-1])
+        ]
+    }
 
     response = agent.invoke(state)
 
-    messages = response.get("messages")
+    messages = response.get("messages", [])
 
-    ai_messages = [message.content for message in messages if isinstance(message,AIMessage)]
+    ai_messages = [
+        message.content
+        for message in messages
+        if isinstance(message, AIMessage)
+    ]
 
-    return ai_messages[-1]
-
-
-
-
-
-
+    return ai_messages[-1] if ai_messages else "No response generated."
